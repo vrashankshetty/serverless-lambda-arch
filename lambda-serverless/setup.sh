bf7f009761cc661b2ab59e88154bf01a004d03b3
VrashankShetty <117751353+vrashankshetty@users.noreply.github.com>
2025-09-21 22:08:05 +0530

    update setup.sh

--- lambda-serverless/setup.sh
+++ lambda-serverless/setup.sh
@@ -4,7 +4,9 @@ sudo apt-get install -y \
         curl \
         gnupg \
         lsb-release \
-        git
+        git \
+        npm \
+        nginx
   
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -21,3 +23,16 @@ sudo apt-get install -y git
       # Start Docker service
 sudo systemctl enable docker
 sudo systemctl start docker
+
+#mongo db setup
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.2.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
